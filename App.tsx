1f17ea12fa4d13a5f14c6a5276c4bebd297e1486
hetansh.patel <hetansh.patel@innvonix.com>
2024-08-06 17:34:45 +0530

    Init project.

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <Text style={styles.mainText}>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mainText: {
+    fontSize: 30,
+    color: "black",
+  },
+});
